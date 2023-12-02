a6ca5d1bc0150ebbf7d1642126f1d1a26b920e0e
Stefan Engström <stefaneng86@gmail.com>
2023-12-02 15:59:42 +0100

    day2

---
+++ day1.py
@@ -0,0 +1,51 @@
+import sys
+import itertools
+import functools
+import copy
+from typing import Iterable
+import subprocess
+from collections import Counter
+from collections import defaultdict
+from collections import namedtuple
+import re
+import aoc
+import numpy as np
+import networkx as nx
+import operator
+import time
+
+vecadd = lambda *v: tuple(sum(x) for x in zip(*v))
+sys.setrecursionlimit(10000)
+
+day = '2'
+p = subprocess.run("bash -c './p_data.sh " + day + " true' ")
+input = open('./input_d_' + day + '.txt').read()
+
+
+lines=[e for e in input.split('\n')]
+
+#REGEX
+#pattern = r'.*?([0-9-]+).*?([0-9-]+).*?([0-9-]+).*?([0-9-]+).*?([0-9-]+).*?([0-9-]+).*?([0-9-]+).*?'
+#parsed = [(int(bid),int(ore),int(clay),int(obo),int(obc),int(geo),int(geob)) for bid,ore,clay,obo,obc,geo,geob in re.findall(pattern,input)]
+
+
+#MATRIX
+col_length=len(lines)-1
+row_length=max([len(lines[c]) for c in range(0,col_length)])
+print(col_length, row_length)
+#matrix=[[list(lines[y])[x] if len(list(lines[y]))>x else " " for x in range(0,row_length)] for y in range(0,col_length)]
+#matrix=[[x for x in range(0,row_length)] for y in range(0,col_length)]
+#print(matrix)
+
+print(input)
+#PARSE SIMPLE
+for line in lines[0:]:
+    parts=line.split(' ')
+    #x,y=parts[0],parts[1]
+    #print(x,y)
+
+
+
+
+
+
