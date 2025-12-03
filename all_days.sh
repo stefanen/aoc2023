a5b6a3e8f9f5a3fd44bd08356c02f3fc8afad491
Stefan Engström <stefaneng86@gmail.com>
2025-12-03 16:48:52 +0100

    d3

--- all_days.sh
+++ all_days.sh
@@ -9,7 +9,8 @@ bash p_data.sh 2 | awk '{print NR"_ "$0;for (j=1;j<=NF;j++) {s="";for (i=1;i<=NF
 cat input3.txt | egrep -o "mul\([0-9]+,[0-9]+\)|do\(\)|don't\(\)" | sed -e 's/^don.*/99999/g' | sed -e 's/^do.*/00000/g' | sed -e 's/[^0-9,]//g' | awk -F, '{if ($1=="99999") d=1;if ($1=="00000") d=0; if (d==0 && $2!="") print $0}' | awk -F, '{s+=$1*$2} END {print s}'
 cat input3.txt | egrep -o "mul\([0-9]+,[0-9]+\)" | sed -e 's/[^0-9,]//g' | awk -F, '{s+=$1*$2} END {print s}'
 
-#day4 later
 
 bash p_data.sh 5 | head -n 1176 | awk -F'|' '{print $2"#"$1}' | tr '\n' '|' | sed -e 's/.$//' | xargs -I% egrep -v % <(bash p_data.sh 5 | tail -n 197 | awk -F, '{s=$((NF+1)/2)"_";for (i=1; i<=NF;i++) {for (j=i+1; j<=NF;j++) s=s$i"#"$j}; print s}') | awk -F_ '{s+=$1; print s}'
+#day5p2 see cmd.sh
+
 
--- day1.py
+++ day1.py
@@ -1,3 +1,4 @@
+import math
 import sys
 import itertools
 import functools
@@ -8,7 +9,6 @@ from collections import Counter
 from collections import defaultdict
 from collections import namedtuple
 import re
-import aoc
 import numpy as np
 import networkx as nx
 import operator
@@ -17,10 +17,10 @@ import time
 vecadd = lambda *v: tuple(sum(x) for x in zip(*v))
 sys.setrecursionlimit(10000)
 
-day = '2'
-p = subprocess.run("bash -c './p_data.sh " + day + " true' ")
+day = '1'
+#p = subprocess.run("bash -c './p_data.sh " + day )
 input = open('./input_d_' + day + '.txt').read()
-
+#input = open('./1.txt').read()
 
 lines=[e for e in input.split('\n')]
 
@@ -34,17 +34,55 @@ col_length=len(lines)-1
 row_length=max([len(lines[c]) for c in range(0,col_length)])
 print(col_length, row_length)
 #matrix=[[list(lines[y])[x] if len(list(lines[y]))>x else " " for x in range(0,row_length)] for y in range(0,col_length)]
-#matrix=[[x for x in range(0,row_length)] for y in range(0,col_length)]
+#matrix=[[list(lines[y])[x] for x in range(0,row_length)] for y in range(0,col_length)]
 #print(matrix)
+
+
 
 print(input)
 #PARSE SIMPLE
+
+r=0
+s=50
+prev_c=0
+prev_m = 50
 for line in lines[0:]:
-    parts=line.split(' ')
-    #x,y=parts[0],parts[1]
-    #print(x,y)
+    if len(line)==0:
+        continue
 
+    
 
+    if line[0]=='R':
+        s+=int(line[1:])
+    else:
+        s-=int(line[1:])
+
+  
+    new_m = s%100
+    new_c = s//100
+
+    diff=new_c-prev_c
+  
+    if new_m != 0 and prev_m != 0:
+        r+=abs(diff)
+    elif new_m == 0 and diff<0:
+        r+=1+abs(diff)
+    elif new_m == 0 and diff>0:
+        r+=1+abs(diff-1)
+    elif new_m == 0 and diff==0:
+        r+=1
+    elif prev_m == 0 and diff<0:
+        r+=abs(diff+1)
+    elif prev_m == 0 and diff>0:
+        r+=abs(diff)
+    else:
+        r+=0 
+
+    prev_m=new_m
+    prev_c=new_c
+
+    print(s,diff, r)
+print(r)
 
 
 
